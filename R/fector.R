5b52c257509dbc281b22c0cd0c54ac380809c422
Dr. Óscar Crespo Salvador <crespo@ad.uniklinik-freiburg.de>
2022-06-22 13:27:03 +0200

    fector actualizado para poder separar también por otros símbolos... espero que no de problemas porque es mi función más básica

--- R/fector.R
+++ R/fector.R
@@ -1,20 +1,23 @@
-# fector: Funci?n para hacer vetores facilmente, escribo entre comillas los elementos separados por comas con o sin espacios y me los da como un vector separado de verdad. Con la actualizaci?n, puedo poner comas o espacios repetidos y/o comas iniciales y finales y me las elimina
-#' Title
+#' Crear vectores facilmente a partir de texto
+#'
+#' Función para hacer vetores facilmente, escribo entre comillas los elementos separados por comas (o lo que especifique en el argumento 'sep') con o sin espacios y me los da como un vector separado de verdad. Con la actualización, puedo poner separadores o espacios repetidos y/o separadores iniciales y finales y me los elimina
 #'
 #' @param VECTOR
+#' @param sep Por defecto es una coma, pero puede ser cualquier cosa. Lo creé para los modelos que vienen separados por un +
 #'
 #' @return
 #' @export
 #'
 #' @examples
-fector <- function(VECTOR) {
-  patrones <- c(',{2,}', ', +,')
+fector <- function(VECTOR, sep = ',') {
+  sep <- paste0('\\',sep)
+  patrones <- c(paste0(sep,'{2,}'), paste0(sep,' +',sep))
   for (PATRON in patrones) {
-    TEMP <- gsub(pattern = PATRON, replacement = ',' , x = VECTOR)
+    TEMP <- gsub(pattern = PATRON, replacement = sep , x = VECTOR)
     VECTOR <- TEMP
   }
-  VECTOR <- gsub(pattern = ',$', replacement = '', x =
-                   gsub(pattern = '^,', replacement = '', x = VECTOR))
-  vectorizado <- unlist(stringr::str_split(string = VECTOR, pattern = ' *, *'))
+  VECTOR <- gsub(pattern = paste0(sep,'$'), replacement = '', x =
+                   gsub(pattern = paste0('^',sep), replacement = '', x = VECTOR))
+  vectorizado <- unlist(stringr::str_split(string = VECTOR, pattern = paste0(' *',sep,' *')))
   return(vectorizado)
 }
