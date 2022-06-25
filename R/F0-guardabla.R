f0bae6f0563c305d84c4f6b88761f29e6cc96be8
elOscatron <83847189+elOscatron@users.noreply.github.com>
2022-06-25 20:59:03 +0200

    corregida guardabla y editado el template

--- R/F0-guardabla.R
+++ R/F0-guardabla.R
@@ -2,8 +2,8 @@
 #'
 #' En desarrollo, especifico que tabla quiero guardar
 #'
-#' @param TABLAe La tabla que voy a guardar
-#' @param donde Por defecto ser· el directorio de trabajo, si no, se puede especificar donde se quiere que sea. Si le falta la barra final '/', se la aÒade solo
+#' @param nombreT El nombre de la tabla que voy a guardar
+#' @param donde Por defecto ser√° el directorio de trabajo, si no, se puede especificar donde se quiere que sea. Si le falta la barra final '/', se la a√±ade solo
 #' @param formatoT Por defecto "(%Y-%m-%d-%H-%M)", especifica el formato de la fecha que se quiere poner
 #' @param formatoA Por defecto 'xlsx', se puede poner ptencialmente otros. Si falta el punto se le pone
 #'
@@ -11,13 +11,12 @@
 #' @export
 #'
 #' @examples
-guardabla <- function(TABLAe, donde = NA, formatoT = "(%Y-%m-%d-%H-%M)", formatoA = 'xlsx') {
+guardabla <- function(nombreT, donde = NA, formatoT = "(%Y-%m-%d-%H-%M)", formatoA = 'xlsx') {
   if(is.na(donde)) {donde <- getwd()}
   if(!grepl(pattern = '/$', x = donde)) {donde <- paste0(donde,'/')}
-  nombre <- deparse(quote(TABLAe))
   tiempo <- format(Sys.time(), format = formatoT)
   if(!grepl(pattern = '\\.',x = formatoA)) {
     formatoA <- paste0('.',formatoA)}
   if(grepl(pattern = 'xlsx', x = formatoA)) {
-    xlsx::write.xlsx(x = TABLAe, file = paste0(donde, nombre, tiempo, formatoA))}
+    xlsx::write.xlsx(x = get(nombreT), file = paste0(donde, nombreT, tiempo, formatoA))}
 }
