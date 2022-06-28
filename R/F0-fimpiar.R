fe3d087ba428ea22c3eed7771574ff5cb3bef6ab
Dr. Óscar Crespo Salvador <crespo@ad.uniklinik-freiburg.de>
2022-06-28 15:52:39 +0200

    fimpiar arreglado otra vez

--- R/F0-fimpiar.R
+++ R/F0-fimpiar.R
@@ -1,5 +1,7 @@
 #' limpiar área de trabajo de elementos previos
 #'
+#' Permite archivos que tienen el mismo nombre (simple, sin guiones o mierdas) peor más largo, porque ordena el vector de más largo a menos
+#'
 #' @param donde Por defecto el directorio de trabajo. Nombre de la carpeta vieja a crear
 #' @param viejpeta Por defecto Viejo, nombre de la carpeta que se creará si no hay una compatible
 #' @param nomviej Elementos a buscar para detectar cosas viejas. No lo pongo en inglés por si acaso hay problemas
@@ -9,7 +11,7 @@
 #' @export
 #'
 #' @examples
-fimpiar <- function(donde = NULL, viejpeta = 'Viejo', nomviej = 'viejo|vieja', aextraer = c('(\\d{4}-\\d{2}-\\d{2}-\\d{2}-\\d{2}-\\d{2})', '(\\d{4}-\\d{2}-\\d{2}-\\d{2}-\\d{2})', '(\\d{4}-\\d{2}-\\d{2}-\\d{2})','\\(\\d{4}-\\d{2}-\\d{2}\\)', '\\d{4}-\\d{2}-\\d{2}',' - Copy','.[a-zA-Z]{1,4}$','\\([a-z]\\)','^[a-z]{1} ',' \\.','^ {1,}', ' {1,}$', ' {2,}', '\\d-', '_[a-zA-Z]{0,}$')) {
+fimpiar <- function(donde = NULL, viejpeta = 'Viejo', nomviej = 'viejo|vieja', aextraer = c('(\\d{4}-\\d{2}-\\d{2}-\\d{2}-\\d{2}-\\d{2})', '(\\d{4}-\\d{2}-\\d{2}-\\d{2}-\\d{2})', '(\\d{4}-\\d{2}-\\d{2}-\\d{2})','\\(\\d{4}-\\d{2}-\\d{2}[a-zA-Z]{0,}\\)', '\\d{4}-\\d{2}-\\d{2}[a-zA-Z]{0,}',' - Copy','.[a-zA-Z]{1,4}$','\\([a-z]\\)','^[a-z]{1} ',' \\.','^ {1,}', ' {1,}$', ' {2,}', '\\d-', '_[a-zA-Z]{0,}$')) {
   if (is.null(donde)) {
     donde <- paste0(getwd(), '/')
   } else {
@@ -37,8 +39,8 @@ fimpiar <- function(donde = NULL, viejpeta = 'Viejo', nomviej = 'viejo|vieja', a
     VECTORm <- if (length(VECTORm) == 0) {VECTORe2} else {VECTORm}
     VECTORm <- unique(gsub(pattern =  ELEMENTO, replacement = '', x = VECTORm, ignore.case = TRUE))
   }
-  # Vector con los nombres simples
-  vEA <- VECTORm
+  # Vector con los nombres simples, los ordeno de más largo a menos para que si hay alguno que se llame igual pero sea más largo no se vea jodido por los otros
+  vEA <-VECTORm[order(nchar(VECTORm), VECTORm, decreasing = TRUE)]
   impringar('Hay ', length(vEA), ' nombres simples que son: ')
   print(vEA)
 
@@ -58,20 +60,26 @@ fimpiar <- function(donde = NULL, viejpeta = 'Viejo', nomviej = 'viejo|vieja', a
   }
 
   # Meto los archivos viejos en la carpeta de viejos
-  TABLAe <- archivosE
+  TABLAm <- archivosE
   VECTORe3 <- vEA
+  VECTORs <- c()
 
-  for (elemento in seq(length(VECTORe3))) { # Recorro cada elemento simplificado
+  suppressWarnings(for (elemento in seq(length(VECTORe3))) { # Recorro cada elemento simplificado
     ELEMENTO <- VECTORe3[elemento]
+    #ELEMENTO <- vEA[5]
     # Creo una subtabla de los archivos que tienen el nombre simplificado
-    TABLAt <- TABLAe[grep(pattern = ELEMENTO, x = TABLAe$Nombres, ignore.case = FALSE),]
+    TABLAt <- TABLAm[!TABLAm$Nombres %in% VECTORs,]
+    TABLAt <- TABLAt[grep(pattern = ELEMENTO, x = TABLAt$Nombres, ignore.case = FALSE),]
     impringar("Para el elemento '", ELEMENTO, "' se han encontado ", nrow(TABLAt)," archivos:")
     print(TABLAt$Nombres)
     # Selecciono el que sea más nuevo
     ELEGIDO <- TABLAt$Nombres[which.max(TABLAt$mtime)]
+    VECTORs <- append(VECTORs, ELEGIDO)
+    # Elimino el elegido de la tabla para que no se lo lleve
+    #    TABLAm <- TABLAm[which(TABLAm$Nombres != ELEGIDO),]
     impringar("El archivo elegido es '", ELEGIDO,"'.")
     # Guardo los nombres del resto
-    DESCARTADOS <- rownames(TABLAt[!TABLAt$Nombres %in% ELEGIDO,])
+    DESCARTADOS <- rownames(TABLAt[!TABLAt$Nombres %in% VECTORs,])
     # Muevo el resto a la carpeta de los viejos
     if (length(DESCARTADOS) != 0){
       file.copy(from = DESCARTADOS, to = viejfin)
@@ -79,5 +87,5 @@ fimpiar <- function(donde = NULL, viejpeta = 'Viejo', nomviej = 'viejo|vieja', a
       file.remove(DESCARTADOS)
     } else {print('No había versiones previas, no se ha movido nada.')}
     cat('\n')
-  }
+  })
 }
