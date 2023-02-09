abf6baad8c835384a99c332ab8751e1d7778c5d3
Dr. Óscar Crespo Salvador <crespo@ad.uniklinik-freiburg.de>
2023-02-09 15:04:07 +0100

    Arreglada la función, le he quitado la opción de openxlsx porque generaba una tabla de una celda

--- R/F0-guardabla.R
+++ R/F0-guardabla.R
@@ -2,7 +2,7 @@
 #'
 #' En desarrollo, especifico que tabla quiero guardar
 #'
-#' @param nombreT El nombre de la tabla que voy a guardar
+#' @param nombreT la tabla que voy a guardar, usará el nombre del objeto
 #' @param donde Por defecto será el directorio de trabajo, si no, se puede especificar donde se quiere que sea. Si le falta la barra final '/', se la añade solo
 #' @param formatoT Por defecto "(%Y-%m-%d-%H-%M)", especifica el formato de la fecha que se quiere poner
 #' @param formatoA Por defecto 'xlsx', se puede poner ptencialmente otros. Si falta el punto se le pone
@@ -12,11 +12,12 @@
 #'
 #' @examples
 guardabla <- function(nombreT, donde = NA, formatoT = "(%Y-%m-%d-%H-%M)", formatoA = 'xlsx') {
+  vNOM <-deparse(substitute(nombreT))
   if(is.na(donde)) {donde <- getwd()}
   if(!grepl(pattern = '/$', x = donde)) {donde <- paste0(donde,'/')}
   tiempo <- format(Sys.time(), format = formatoT)
   if(!grepl(pattern = '\\.',x = formatoA)) {
     formatoA <- paste0('.',formatoA)}
   if(grepl(pattern = 'xlsx', x = formatoA)) {
-    openxlsx::write.xlsx(x = get(nombreT), file = paste0(donde, nombreT, tiempo, formatoA))}
-}
+    write.xlsx(x = nombreT, file = paste0(donde, vNOM, tiempo, formatoA))}
+  }
