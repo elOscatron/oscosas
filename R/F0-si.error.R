27aab49b7003dbd471d8539f6c6897b8f91a3aeb
Dr. Óscar Crespo Salvador <crespo@ad.uniklinik-freiburg.de>
2023-02-10 15:16:47 +0100

    Amplicaciones y cambios en la función si.error. Ya no va a ser compatible con la versión previa, lo cual no es muy preocupante, poeruq no creo que la usara mucho, debido a que no la entendía ni yo

--- R/F0-si.error.R
+++ R/F0-si.error.R
@@ -1,19 +1,38 @@
 #' Detecta errores
-#' equivalente a su función homónima de excel. Es la segunda vez que la hago porque la borr? sin querer. Esta vez la he juntado con ESERROR tambi?n de excel porque con la opci?n de un mensaje personalizado o funci?n, valen para lo mismo. Me he basado en esto: https://stackoverflow.com/questions/31214361/what-is-the-r-equivalent-for-excel-iferror
+#' equivalente a su función homónima de excel. Es la segunda vez que la hago porque la borrE sin querer.
+#' Esta vez la he juntado con ESERROR tambiEn de excel porque con la opciOn de un mensaje personalizado
+#' o funciOn, valen para lo mismo.
+#' Me he basado en esto: https://stackoverflow.com/questions/31214361/what-is-the-r-equivalent-for-excel-iferror
+#'
+#' ChatGPT: La función si.error tiene como objetivo ejecutar el código especificado en intento y controlar
+#' si produce algún error o no. Si se produce un error o el resultado es de longitud cero, la función
+#' devuelve EsError. Si no se produce un error, la función devuelve intento o, si se especifica,
+#' NoEsError. La ejecución del código se hace con la función try, y se establece el argumento silent
+#' en TRUE para evitar que se muestren los mensajes de error en la consola.
 #'
 #' @param intento Qué cosa se quiere probar
-#' @param alternativa Que hacer si la cosa falla
-#' @param mensajeNEG Mensaje que dar si la cosa da error, por defecto TRUE
+#' @param EsError  Anteriormente llamado Alternativa. Código a correr si la cosa falla.
+#' @param NoEsError Anteriormente llamado mensajeNEG. Código a correr, si la cosa NO da error, por defecto NA. Aunque aquí
+#' haya algo que no sea NA, la función principal se ejecutará y si crea un objeto, este se creará
+#' @param warnings Por defecto FALSe, si se pone TRUE, las advertencias se tratarán como errores también
 #'
 #' @return
 #' @export
 #'
 #' @examples
-si.error <- function(intento, alternativa = TRUE, mensajeNEG = NA) {
-  OBJt <- try(intento, silent = TRUE)
-  if (class(OBJt) == "try-error" | length(OBJt) == 0) {alternativa} else {
-    if (is.na(mensajeNEG)) {intento
+si.error <- function(intento, EsError = TRUE, NoEsError = NA, warnings = FALSE) {
+  OBJt <- suppressWarnings(try(intento, silent = TRUE))
+  warning_msg <- NULL
+  if (warnings) {
+    warning_msg <- capture.output(suppressWarnings(intento))
+    if (length(warning_msg) > 0) {
+      OBJt <- suppressWarnings(try(invisible(force(intento)), silent = TRUE))
+    }}
+  if (class(OBJt) == "try-error" || length(OBJt) == 0 || length(warning_msg) > 0) {
+    EsError
+  } else {
+    if (is.na(NoEsError)) {
+      intento
     } else {
-      mensajeNEG}}
-}
-
+      NoEsError
+    }}}
