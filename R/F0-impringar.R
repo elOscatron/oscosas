65457ff262b602bb176550bc448b3153c56638dd
Dr. Óscar Crespo Salvador <crespo@ad.uniklinik-freiburg.de>
2023-07-26 10:52:44 +0200

    añadidas opciones a impringar: salto previo y separadores posteriores y previos

--- R/F0-impringar.R
+++ R/F0-impringar.R
@@ -6,19 +6,30 @@
 #' @param collapse
 #' @param recycle0
 #' @param verbose Si quiero que la cosa simplemente se imprima o que se guarde
-#' @param salto Si quiero añadir un salto de linea, por defecto no
+#' @param presalto Si quiero añadir un salto de linea previo a la impresión, por defecto no
+#' @param salto Si quiero añadir un salto de linea posterior a la impresión, por defecto no
+#' @param presep Si quiero añadir un separador antes de la impresión (y después del presalto). Por defecto NULL, debe ser un vector.
+#' @param sep Si quiero añadir un separador después de la impresión (y antes del salto). Por defecto NULL, debe ser un vector.
+#'
 #'
 #' @return El vector pegado
 #' @export
 #'
 #' @examples
-impringar <- function(..., salto = FALSE, collapse = NULL, recycle0 = FALSE, verbose = TRUE) {
+impringar <- function(..., presep = NULL, sep = NULL, presalto = FALSE, salto = FALSE, collapse = NULL, recycle0 = FALSE, verbose = TRUE) {
   # Guardo un vector con los elementos pegados usando los brillantes argumentos originales de paste0
   VECTORs <- paste0(..., collapse = collapse, recycle0 = recycle0)
   # Si verbose es cierto simplemente imprimo el vector, si no, lo devuelvo para un objeto u otra función
   if (verbose) {
+    if(presalto) { # Si añado un salto previo
+      cat('\n')}
+    if(!is.null(presep)) { # Si añado un separador previo
+      print(presep)}
     print(VECTORs)
-    if(salto) {cat('\n')
-      }} else {
-      return(VECTORs)}
+    if(!is.null(sep)) { # Si añado un separador posterior
+      print(sep)}
+    if(salto) { # Si añado un salto posterior
+      cat('\n')}
+  } else {
+    return(VECTORs)}
 }
